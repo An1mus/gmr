55610f7e8a1835dcd38276d3d054b9521a52b0a3
an1mus <1stallways@gmail.com>
2019-12-13 15:25:09 +0200

    Clean app setup

---
+++ src/components/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        <h1>App is running</h1>
+    </div>
+  );
+};
+
+export default App;
