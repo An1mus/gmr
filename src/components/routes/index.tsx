3f2a23510066995832aedc3a6b4b5c5a6095f912
an1mus <1stallways@gmail.com>
2019-12-19 15:36:28 +0200

    Added routring

---
+++ src/components/routes/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+const Routes = () => {
+    return (
+        <>
+            <Switch>
+                <Route />
+            </Switch>
+        </>
+    );
+};
+
+export default Routes;
