455567001fd53ba376f8ed7c29819f5be74a3059
an1mus <1stallways@gmail.com>
2019-12-24 16:14:37 +0200

    TicTacToe filed added

--- src/components/games/index.tsx
+++ src/components/games/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-import Game from './container';
-
-import TicTacToe from './implementations/tictactoe';
-
 const Games = () => {
     const links = [
         {
