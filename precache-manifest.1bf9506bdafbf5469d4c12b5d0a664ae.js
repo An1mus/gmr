b31fb9e4089f742d24ecc5c805920ca40e21587d
an1mus <1stallways@gmail.com>
2019-12-26 20:42:03 +0200

    Updates

---
+++ precache-manifest.1bf9506bdafbf5469d4c12b5d0a664ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdef6dda85c1e012a9ba91a41b3312d",
+    "url": "/gmr/index.html"
+  },
+  {
+    "revision": "08f70c1c262b6c87dd61",
+    "url": "/gmr/static/css/main.bbb3a616.chunk.css"
+  },
+  {
+    "revision": "041331ede4484130a3f7",
+    "url": "/gmr/static/js/2.f69fd1f7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/gmr/static/js/2.f69fd1f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08f70c1c262b6c87dd61",
+    "url": "/gmr/static/js/main.5ceb48f4.chunk.js"
+  },
+  {
+    "revision": "63f75ce86f747aac59d7",
+    "url": "/gmr/static/js/runtime-main.bb376414.js"
+  }
+]);
