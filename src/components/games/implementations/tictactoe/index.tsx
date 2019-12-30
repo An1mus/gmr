ab79119119471a6687c51338e957b26c1945111e
an1mus <1stallways@gmail.com>
2019-12-31 00:24:10 +0200

    Adding game history

--- src/components/games/implementations/tictactoe/index.tsx
+++ src/components/games/implementations/tictactoe/index.tsx
@@ -8,28 +8,31 @@ import GameResult from './components/result';
 
 const TicTacToe = () => {
     const emptyField = new Array(9).fill({type: CELL_TYPES.EMPTY});
-
-    let [field, setField] = useState(emptyField);
-    let [gameState, setGameState] = useState(GAME_STATES.PLAYING);
-    let [stepType, setStepType] = useState(CELL_TYPES.CROSS);
+    const [winHistory, setWinHistory] = useState([]);
+    const [field, setField] = useState(emptyField);
+    const [gameState, setGameState] = useState(GAME_STATES.PLAYING);
+    const [stepType, setStepType] = useState(CELL_TYPES.CROSS);
 
     // TODO: use memoization
-    // TODO: add game history component
     useEffect(() => {
         const reducedState = field.reduce((p, el) => p + el.type, '');
 
         const circles: string = reducedState.split(CELL_TYPES.CROSS).join('.').split(CELL_TYPES.EMPTY).join('.');
         const crosses: string = reducedState.split(CELL_TYPES.CIRCLE).join('.').split(CELL_TYPES.EMPTY).join('.');
         const fieldFull = reducedState.indexOf(CELL_TYPES.EMPTY) < 0;
-        console.log(circles, WINING_PATTERNS[circles]);
-        console.log(crosses, WINING_PATTERNS[crosses]);
 
         const gameWon = WINING_PATTERNS[circles] || WINING_PATTERNS[crosses];
 
-        if (gameWon) return setGameState(gameWon);
-        if (fieldFull) return setGameState(GAME_STATES.FIELD_FULL);
+        if (gameWon) {
+            // TODO: add game history component
+            // setWinHistory((prevState) => [...prevState, gameWon]);
+            setGameState(gameWon);
+        }
+        if (fieldFull) {
+            return setGameState(GAME_STATES.FIELD_FULL);
+        }
 
-    }, [field]);
+    }, [field, winHistory]);
 
     const turnClick = (index: number): void => {
         if (gameState !== GAME_STATES.PLAYING) return;
@@ -50,8 +53,8 @@ const TicTacToe = () => {
 
     return (
         <>
-            <Field field={field} turnClick={turnClick}/>
             <GameResult stepType={stepType} gameState={gameState} refresh={refreshGame}/>
+            <Field field={field} turnClick={turnClick}/>
         </>
     );
 };
