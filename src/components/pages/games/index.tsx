8fa4330fe1a2102e5c5f9c861026e222b7b9733b
an1mus <1stallways@gmail.com>
2020-01-10 16:15:31 +0200

    Gmaes nav icon

--- src/components/pages/games/index.tsx
+++ src/components/pages/games/index.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 import { HashRouter, NavLink, Route, Switch } from 'react-router-dom';
 import { GAME_LINKS, GAME_COMPONENTS } from './config/games';
+
+const GameNav = styled.nav`
+    display: flex;
+    justify-content: start;
+`;
+
+const GameLink = styled.div`
+    width: 2.5rem;
+
+    svg {
+        position: relative;
+        transition: fill .3s;
+        fill: var(--main-font-color);
+    }
+    .active,
+    &:hover{
+        svg {
+            transition: fill .3s;
+            fill: var(--main-highlight-font-color);
+        }
+    }
+`;
 
 const Games = () => {
     return (
         <>
             <h2>Here is some stuff to play</h2>
             <HashRouter>
-                <nav>
-                    {GAME_LINKS.map(link => (<NavLink key={link.id} to={`/games${link.path}`}>{link.title}</NavLink>))}
-                </nav>
+                <GameNav>
+                    {GAME_LINKS.map(link => (
+                        <GameLink>
+                            <NavLink key={link.id} to={`/games${link.path}`} activeClassName={'active'}>
+                                    <link.icon/>
+                            </NavLink>
+                        </GameLink>
+                    ))}
+                </GameNav>
                 <Switch>
                     {GAME_COMPONENTS.map(game => (<Route key={game.id} path={`/games${game.path}`} component={game.component}/>))}
                 </Switch>
