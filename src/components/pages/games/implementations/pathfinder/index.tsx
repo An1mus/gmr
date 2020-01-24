e8b8cdf8e6a735e35c57f8d40537887420f4a2e4
an1mus <1stallways@gmail.com>
2020-01-24 18:55:17 +0200

    Pathfinder start

---
+++ src/components/pages/games/implementations/pathfinder/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PathFinderContainer = styled.div`
+`;
+
+const PathFinder = () => {
+    return (
+        <PathFinderContainer>
+
+        </PathFinderContainer>
+    );
+};
+
+export default PathFinder;
