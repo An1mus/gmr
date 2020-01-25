cc9112a1b6195451c13a82ad96598ad7446de9a6
an1mus <1stallways@gmail.com>
2020-01-25 18:23:23 +0200

    Adding pong

--- src/components/pages/games/config/games.tsx
+++ src/components/pages/games/config/games.tsx
@@ -1,11 +1,20 @@
 import {ReactComponent as TicTacToeIcon} from '../../../../assets/game-nav-icons/ttt.svg';
+import {ReactComponent as PongIcon} from '../../../../assets/game-nav-icons/ping-pong.svg';
+
 import TicTacToe from '../implementations/tictactoe';
+import Pong from '../implementations/pong';
 
 const GAME_LINKS = [
     {
         id: 0,
         path: '/ttt',
         icon: TicTacToeIcon,
+        title: 'Tic Tac Toe'
+    },
+    {
+        id: 1,
+        path: '/pong',
+        icon: PongIcon,
         title: 'Tic Tac Toe'
     }
 ];
@@ -15,6 +24,11 @@ const GAME_COMPONENTS = [
         id: 0,
         path: '/ttt',
         component: TicTacToe,
+    },
+    {
+        id: 1,
+        path: '/pong',
+        component: Pong,
     }
 ];
 
---
+++ src/components/pages/games/implementations/pong/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PongContainer = styled.div`
+`;
+
+const Pong = () => {
+    return (
+        <PongContainer>
+            <h2>Pong</h2>
+        </PongContainer>
+    );
+};
+
+export default Pong;
