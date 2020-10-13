09a6dcd6efaf3e2bb37479b51b8406e50043c2ac
An1mus <an1mus@icloud.com>
2020-10-13 17:09:33 +0300

    Styled component for  Visualisations section

--- src/components/pages/visualisations/index.tsx
+++ src/components/pages/visualisations/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const VisualisationsContainer = styled.div``;
+
 const Visualisations = () => {
-    return (
-        <>
-        </>
-    );
+    return <VisualisationsContainer>
+    </VisualisationsContainer>
 };
 
 export default Visualisations;
